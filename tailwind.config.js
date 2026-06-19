9695245366dc5a22e4b905755fdee31758e54df0
Simon <sjesimon@gmail.com>
2026-06-19 21:29:25 +0000

    Tailwind Update to remove FOUC issue

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './members.html'],
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          DEFAULT: '#0A2540',
+          light: '#1E3A5F',
+        },
+        gold: '#C9A227',
+        sage: '#7C8F7E',
+        ivory: '#FDFBF7',
+        cream: '#F5F0E6',
+      },
+      fontFamily: {
+        serif: ['"Playfair Display"', 'Georgia', 'serif'],
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
